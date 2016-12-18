88ebdad9856c367f6c3171c3444eee5755e887cf
cfrancobedoya <cfrancobedoya@gmail.com>
2016-12-18 17:17:25 -0500

    CONEXIÓN CON EL SERVIDOR
    Se establece la conexión con el servidor por medio de una vista
    estática con express.

--- sever/index.js
+++
@@ -1,9 +0,0 @@
-var app = express();
-var server = require('https').Server(app);
-var fs = require('fs');
-
-app.use(express.static('public'));
-
-server.listen(3000, function(io){
-	console.log("Servidor corriendo en http://localhost:3000");
-})
