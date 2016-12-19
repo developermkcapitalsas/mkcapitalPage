254267cd89ed09983061235eb5a1cc429422b528
cfrancobedoya <cfrancobedoya@gmail.com>
2016-12-18 21:30:10 -0500

    Cambio de variable port
    Se cambia la variable port a variable de ambiente o puerto 8080

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express'),
 app = express();
+var port = process.env.PORT || 8080;
 app.use('/', express.static(__dirname + '/'));
-app.listen(8080);
+app.listen(port);
+
+console.log('Aplicación corriendo en: http://localhost:' + port);
