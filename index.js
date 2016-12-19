667579a1cabfa35123f71a7965a7f8f47ed42feb
cfrancobedoya <cfrancobedoya@gmail.com>
2016-12-18 22:21:10 -0500

    CAMBIO DE PUERTO
    Debido a desarrollo local, se cambia el puerto de 8080 a 5000.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express'),
-app = express();
-var port = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
 app.use('/', express.static(__dirname + '/'));
 app.listen(port);
 
