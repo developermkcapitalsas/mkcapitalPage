37c8aacc2c06fd4d23580d1d4f9a15994cf9b8ea
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-01 16:28:13 -0500

    ARREGLOS PALABRAS
    Cambios en algunas palabras.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const config = require('./config')
 
 app.listen(config.port , function (){
     console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
-    console.log("Desarrollado por: Cristian Franco Bedoya");
-    console.log("Franco Software 2016");
+    console.log("Desarrollado por: Ramla Pro. Software");
+    console.log("Ramla Pro. Software 2016");
+    console.log("Dueño del producto: MK Capital S.A.S");
 });
